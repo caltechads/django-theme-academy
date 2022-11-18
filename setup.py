608a3b6e20f68ee3f8c107aed6e74c7608e2c077
Glenn Bach <glenn@caltech.edu>
2022-11-18 15:30:24 -0800

    Bump version: 0.2.0 → 0.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="django-theme-academy",
-    version="0.2.0",
+    version="0.2.1",
     packages=find_packages(),
     include_package_data=True,
     package_data={'academy_theme': ["py.typed"]},
