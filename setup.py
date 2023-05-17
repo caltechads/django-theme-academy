8948b53fded367718e517b080be4aa58b4f2a915
Chris Malek <cmalek@caltech.edu>
2023-05-17 11:42:43 -0700

    Bump version: 0.3.4 → 0.3.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding='utf-8') as fh:
 
 setup(
     name="django-theme-academy",
-    version="0.3.4",
+    version="0.3.5",
     packages=find_packages(),
     include_package_data=True,
     package_data={'academy_theme': ["py.typed"]},
